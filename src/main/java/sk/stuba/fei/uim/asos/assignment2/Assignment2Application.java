ae69c3ef6e6c8926e679ca7a180b4518319bb052
Milan Mladoniczky <6153201+mladoniczky@users.noreply.github.com>
2020-11-08 21:44:17 +0100

    - add pom - create folder structure - add assignment

---
+++ src/main/java/sk/stuba/fei/uim/asos/assignment2/Assignment2Application.java
@@ -0,0 +1,12 @@
+package sk.stuba.fei.uim.asos.assignment2;
+
+import lombok.extern.java.Log;
+
+@Log
+public class Assignment2Application {
+
+    public static void main(String[] args) {
+        log.info("Application started");
+    }
+
+}
